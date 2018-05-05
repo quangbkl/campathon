d30de7acac4d25be0ce603c3cc32807022f919e4
Doan Van Quang <quang99dev@gmail.com>
2018-05-05 15:24:47 +0700

    Login Facebook Google

--- src/app/App.js
+++ src/app/App.js
@@ -8,6 +8,8 @@ import Header from "./shared-components/header/Header";
 import LoginPage from "./pages/login/LoginPage";
 import RegisterPage from "./pages/register/RegisterPage";
 import ProfilePage from "./pages/profile/ProfilePage";
+import Footer from "./shared-components/footer/Footer";
+import FacebookGoogle from "./pages/login/FacebookGoogle";
 
 class App extends Component {
     render() {
@@ -19,10 +21,12 @@ class App extends Component {
                     <Route path="/posts/:id" component={PostDetail}/>
                     <Route path="/upload" component={UploadPage}/>
                     <Route path="/login" component={LoginPage}/>
+                    <Route path="/FacebookGoogle" component={FacebookGoogle}/>
                     <Route path="/register" component={RegisterPage}/>
                     <Route exact path="/profile" component={ProfilePage}/>
                     <Route component={PageNotFound}/>
                 </Switch>
+                <Footer/>
             </div>
         );
     }
