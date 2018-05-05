d8b0ef8b7a1e0646ee7a83c6f5b3facc423eb272
hoangtiendong <hoangtiendong98@gmail.com>
2018-05-05 16:40:31 +0700

    hihi

--- src/app/shared-components/header/Header.js
+++ src/app/shared-components/header/Header.js
@@ -1,70 +1,38 @@
 import React, {Component} from "react";
-import {Link} from "react-router-dom";
-import {addAuthListener, isAuthenticated, removeAuthListener, setAuthState} from "../../../services/AuthServices";
 import "./Header.css";
+import Navbar from "../navbar/Navbar";
+import HomePage from "../../pages/home/HomePage";
+import News from "../../pages/news/News";
+import UploadPage from "../../pages/upload/UploadPage";
+import About from "../../pages/about/About";
+import {Route, Switch} from "react-router-dom";
+import "./Header.css"
 
 class Header extends Component {
-    state = {
-        auth: isAuthenticated(),
-    };
-
-    componentDidMount() {
-        addAuthListener(this._handleOnAuthChange);
-    }
-
-    componentWillUnmount() {
-        removeAuthListener(this._handleOnAuthChange);
-    }
-
-    _handleOnAuthChange = () => {
-        this.setState({
-            auth: isAuthenticated(),
-        })
-    };
 
     render() {
-        const {auth} = this.state;
+
 
         return (
             <div className="Header">
-                <nav className="navbar navbar-expand-sm navbar-light bg-light">
-                    <div className="container d-flex justify-content-between align-items-center">
-                        <Link className="Logo" to="/">
-                            <span>Crush Hunt</span>
-                        </Link>
+                <div className="Logo">
+                    <img src="./logo-u.png" />
+                </div>
 
-                        <form className="SearchForm">
-                            <span className="icon"/>
-                            <input type="text" placeholder="Search"/>
-                        </form>
+                <Navbar/>
 
-                        <div className="RightActions">
-                            <ul className="navbar-nav ml-auto">
-                                {
-                                    !auth &&
-                                    <li className="nav-item">
-                                        <Link to="/login" className="btn btn-outline my-2 my-sm-0">Login</Link>
-                                    </li>
-                                }
-                                {
-                                    auth &&
-                                    <li onClick={this._handleClickLogout} className="nav-item">
-                                        <a className="btn btn-outline-success my-2 my-sm-0">Logout</a>
-                                    </li>
-                                }
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
+                <Switch>
+                    <Route exact path="/" component={HomePage} />
+                    <Route path="/news" component={News} />
+                    <Route path="/upload" component={UploadPage} />
+                    <Route path="/" component={About} />
+                    <Route exact path="/" component={HomePage} />
+
+                </Switch>
             </div>
         );
     }
 
-    _handleClickLogout = () => {
-        setAuthState({
-            accessToken: null
-        });
-    }
 }
 
 export default Header;
