6709774d5091a62a40cc0bc8eaf591691ffb069f
hoangtiendong <hoangtiendong98@gmail.com>
2018-05-05 20:30:59 +0700

    header va footer

--- src/app/pages/about/About.js
+++ src/app/pages/about/About.js
@@ -4,7 +4,9 @@ class About extends Component {
     render(){
         return(
 
-            <div></div>
+            <div>
+
+            </div>
         );
     }
 }
--- src/app/shared-components/header/Header.js
+++ src/app/shared-components/header/Header.js
@@ -1,34 +1,12 @@
 import React, {Component} from "react";
 import "./Header.css";
 import Navbar from "../navbar/Navbar";
-import HomePage from "../../pages/home/HomePage";
-import News from "../../pages/news/News";
-import UploadPage from "../../pages/upload/UploadPage";
-import About from "../../pages/about/About";
-import {Route, Switch} from "react-router-dom";
-import "./Header.css"
-
 class Header extends Component {
 
     render() {
-
-
         return (
             <div className="Header">
-                <div className="Logo">
-                    <img src="./logo-u.png" />
-                </div>
-
                 <Navbar/>
-
-                <Switch>
-                    <Route exact path="/" component={HomePage} />
-                    <Route path="/news" component={News} />
-                    <Route path="/upload" component={UploadPage} />
-                    <Route path="/" component={About} />
-                    <Route exact path="/" component={HomePage} />
-
-                </Switch>
             </div>
         );
     }
