ea53dfa5fbaa810ae1c428ee335206440229b67f
Nguyen <ngokhoaisan1999@gmail.com>
2018-05-05 23:01:30 +0700

    404 page

--- src/app/pages/page-not-found/PageNotFound.js
+++ src/app/pages/page-not-found/PageNotFound.js
@@ -1,14 +1,15 @@
 import React, {Component} from "react";
 import {Link} from "react-router-dom";
+import "./PageNotFound.css";
 
 class PageNotFound extends Component {
     render() {
 
         return (
-            <div className="PageNotFound text-center">
-                <h1>Page not found</h1>
+            <div className="page-not-found">
+                <h1 className="text-404">404</h1>
 
-                <div className="Help">
+                <div className="help">
                     Oops! Looks like this page doesn't exits.<br/>
                     You could return <Link to="/">to the homepage</Link> or or feel free to report this issue.
                 </div>
