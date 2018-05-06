782ca67510012c0c38e8712f64c50f497bf69488
hoangtiendong <hoangtiendong98@gmail.com>
2018-05-06 13:24:10 +0700

    changed

--- src/app/pages/home/HomePage.js
+++ src/app/pages/home/HomePage.js
@@ -2,14 +2,7 @@ import React, {Component} from "react";
 import PropTypes from 'prop-types';
 import "./HomePage.css";
 import ListPost from "./ListPost";
-<<<<<<< HEAD
-import getNewPost from "../../../services/APIServices";
-=======
-
 import getNewPost from "../../../services/APIGetPost";
-
-
->>>>>>> a4f9bf79fb6e7044d507c24ea9d5425756e0c148
 
 class HomePage extends Component {
     constructor(props){
