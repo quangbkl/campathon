8be183e7fe41bb6a7ceb74612b4a6364476e6014
hoangtiendong <hoangtiendong98@gmail.com>
2018-05-06 07:23:01 +0700

    changed

--- src/app/pages/contact/Contact.js
+++ src/app/pages/contact/Contact.js
@@ -17,8 +17,12 @@ class Contact extends Component {
         });
     }
 
+    handleSubmit(e) {
+        e.preventDefault();
+    }
+
     render(){
-        const {email,name,type,content}=this.state;
+        const {email,name,content}=this.state;
         return(
             <div className="Contact">
                 <div className="Title">
@@ -26,9 +30,9 @@ class Contact extends Component {
                 </div>
                 <div className="Form">
                     <form className="FormContact">
-                        <input className="Name" type="text" name="username" placeholder="Tên"
+                        <input className="Name" type="text" name="username" placeholder="Tên" value={name}
                                onChange={this._handleChangeInput.bind(this, "name")}/>
-                        <input className="Email" type="text" name="email" placeholder="Email"
+                        <input className="Email" type="text" name="email" placeholder="Email" value={email}
                                    onChange={this._handleChangeInput.bind(this, "email")}/>
                         <select className="Select" >
                             <option selected disabled>Chọn một liên hệ</option>
@@ -39,9 +43,10 @@ class Contact extends Component {
                             <option value="4">Lỗi hệ thống</option>
                             <option value="5">Khác</option>
                         </select>
-                        <textarea className="Content" placeholder="Nhập nội dung"
+                        <textarea className="Content" placeholder="Nhập nội dung" value={content}
                                   onChange={this._handleChangeInput.bind(this, "content")}/>
-                        <button className="Button" type="text" value="Send">Gửi</button>
+                        <button className="Button" type="text" value="Send"
+                                onClick={this.handleSubmit.bind(this)}>Gửi</button>
                     </form>
                 </div>
             </div>
