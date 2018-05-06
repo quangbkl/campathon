d8864eab6c8392610f76930fd27ed6ea768ba293
Nguyen <ngokhoaisan1999@gmail.com>
2018-05-06 11:00:03 +0700

    api commit

--- src/services/APIServices.js
+++ src/services/APIServices.js
@@ -1,37 +1,16 @@
 
-export const login = (email, password) => {
-    const url = 'https://crush.blogk.xyz/login';
+const  getPostList = () => {
+    const url = 'https://hien-mau-team.herokuapp.com/new-post.php';
     const request = new Request(url, {
         method: 'POST',
         headers: {
             'content-type': 'application/json'
         },
-        body: JSON.stringify({
-            email,
-            password,
-            email: email,
-            password: password,
-        })
+
     });
     return fetch(request).then(response => {
         return response.json();
     });
 };
 
-const  getNewView = () => {
-    const url = 'https://hien-mau-team.herokuapp.com/new-post.php';
-    const request = new Request(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-
-        })
-    });
-    return fetch(request).then(response => {
-        return response.json();
-    });
-};
-
-export default  getNewView;
+export default  getPostList();
